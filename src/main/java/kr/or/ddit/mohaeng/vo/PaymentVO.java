3c24624a5b78377ad5fa8d032e04fe7a086312d3
donggeun <dongkun8130@gmail.com>
2026-01-06 15:36:08 +0900

    [BE] 신동근 / 결제, 결제상세, 환불 vo 생성

---
+++ src/main/java/kr/or/ddit/mohaeng/vo/PaymentVO.java
@@ -0,0 +1,20 @@
+package kr.or.ddit.mohaeng.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class PaymentVO {
+	// 결제
+	
+	private String payNo; 		// 결제키
+	private int memNo; 			// 회원키
+	private int payTotalAmt; 	// 결제금액
+	private int usePoint;		// 사용포인트
+	private String payMethodCd; // 공통코드
+	private Date payDt; 		// 결제일시
+	private String payStatus; 	// 결제상태(Y,N,WAIT)
+	private Date cancelDt; 		// 취소일시
+	private String cancelReason;// 취소사유
+}

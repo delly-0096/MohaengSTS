4b4537f1a4bff0cf0ca7e743c87899a2cd36e18e
donggeun <dongkun8130@gmail.com>
2026-01-09 21:23:37 +0900

    [BE] 신동근 / 날짜 데이터 포맷 재설정

--- src/main/java/kr/or/ddit/mohaeng/vo/FlightProductVO.java
+++ src/main/java/kr/or/ddit/mohaeng/vo/FlightProductVO.java
@@ -16,36 +16,32 @@ public class FlightProductVO {
 	
 	private String fltProdId;     	 // 항공권 키 - 일자 + 순서 (db변경해야됨)
 	
-	private int airlineId;           // 항공사 ID
+	private String airlineId;        // 항공사 ID
 	private String flightSymbol;  	 // 항공편번호
 	private String depAirportId;     // 출발공항 - id(영문 5자리)
 	private String arrAirportId;     // 도착공항 - id(영문 5자리)
 	
-	@DateTimeFormat(pattern = "a HH:mm")
-	@JsonFormat(pattern = "a HH:mm")
+	@JsonFormat(pattern = "yyyyMMdd HH:mm")
 	private LocalDateTime depTime;       // 출발시간
-	@DateTimeFormat(pattern = "a HH:mm")
-	@JsonFormat(pattern = "a HH:mm")
+	@JsonFormat(pattern = "yyyyMMdd HH:mm")
 	private LocalDateTime arrTime;       // 도착시간
 	private String domesticDays;  		 // 월/화/수 등
 	
-	@DateTimeFormat(pattern = "yyyyMMdd")
 	@JsonFormat(pattern = "yyyyMMdd")
 	private LocalDate startDt;         // 운항시작일
-	@DateTimeFormat(pattern = "yyyyMMdd")
 	@JsonFormat(pattern = "yyyyMMdd")
 	private LocalDate endDt;           // 운항종료일
 
 	
-	private int extraBaggagePrice;	// 추가 위탁 수하물 비용 2000원
-	
 	// 수하물 정보
-	private int checkedBaggage;  // 무료 위탁 수하물 - 몇키로 인지입력 15 or 20
-	private int carryOnBaggage = 10;  // 기내 수하물 - 10kg
+	private int extraBaggagePrice = 2000;	// 추가 위탁 수하물 비용 2000원
+	private int checkedBaggage;  		// 무료 위탁 수하물 - 몇키로 인지입력 15 or 20
+	private int carryOnBaggage = 10;	// 기내 수하물 - 10kg
 	
 	
+	//////////////////////////////
 	
-	// 공항 iata코드
+	// 공항 iata코드 (필요없을수도?)
 	private String depIata;			// 출발지 iata코드 ?? 사용여부는 알아서
 	private String arrIata;			// 도착지 iata코드
 

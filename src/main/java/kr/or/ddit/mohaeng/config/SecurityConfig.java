8ec53137ce52f371ec1b3c4799b1d57529d0ccba
kjh <wnsgur5303@gmail.com>
2026-01-12 17:16:57 +0900

    feat: [BE] 김준혁 / 관리자 일정관리 BACKEND 코드 작업 및 설정 수정

--- src/main/java/kr/or/ddit/mohaeng/config/SecurityConfig.java
+++ src/main/java/kr/or/ddit/mohaeng/config/SecurityConfig.java
@@ -108,6 +108,7 @@ public class SecurityConfig {
 	private static final String[] REACT_PASS_URL = {
 			"/api/admin/login",
 			"/api/schedule/**",
+			"/api/admin/schedule/**",
 			"/api/admin/notices/thumbnail/**"
 		};
 

dbade6785400e23fa0c8abf762619b54acdd8ed6
donggeun <dongkun8130@gmail.com>
2026-01-13 18:05:48 +0900

    [BE] 신동근 / 결제키 타입 LONG으로 변환

--- src/main/java/kr/or/ddit/mohaeng/vo/RefundLogVO.java
+++ src/main/java/kr/or/ddit/mohaeng/vo/RefundLogVO.java
@@ -9,7 +9,7 @@ public class RefundLogVO {
 	// 환불
 	
 	private String refundNo; // 환불키
-	private String payNo; // 결제키
+	private int payNo; // 결제키
 	private int refundAmt; // 환불금액
 	private Date regDt; // 환불일시
 	private int refundPoint; // 환불포인트

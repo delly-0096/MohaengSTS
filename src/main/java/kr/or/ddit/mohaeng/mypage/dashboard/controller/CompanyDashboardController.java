79d5c7ccb2e1b78e155398fa2dfc59a4c6a11f22
hyejin <haejina1222@gmail.com>
2026-01-22 17:53:49 +0900

    [BE]이혜진/대시보드

--- src/main/java/kr/or/ddit/mohaeng/mypage/dashboard/controller/CompanyDashboardController.java
+++ src/main/java/kr/or/ddit/mohaeng/mypage/dashboard/controller/CompanyDashboardController.java
@@ -1,26 +1,31 @@
 package kr.or.ddit.mohaeng.mypage.dashboard.controller;
 
+import java.util.List;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.or.ddit.mohaeng.mypage.dashboard.service.CompanyDashboardService;
 import kr.or.ddit.mohaeng.security.CustomUserDetails;
 import kr.or.ddit.mohaeng.vo.CompanyDashboardVO;
+import kr.or.ddit.mohaeng.vo.PaymentVO;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-public class CompanyDashboardConteroller {
+public class CompanyDashboardController {
 
   private final CompanyDashboardService service;
-
+ 
   @ResponseBody
   @GetMapping("/api/company/dashboard")
   public CompanyDashboardVO dashboard(@AuthenticationPrincipal CustomUserDetails user) {
-    int compNo = user.getCompNo();
-    return service.getDashboard(compNo);
+		/* int compNo = user.getCompNo(); */
+	int memNo = user.getMemNo();
+    return service.getDashboard(memNo);
   }
   
   @GetMapping("/mypage/business/products")
@@ -29,8 +34,23 @@ public class CompanyDashboardConteroller {
   }
   
   @GetMapping("/mypage/business/dashboard")
-  		public String dashboardView() {
-	  		return "mypage/business/dashboard";
+  		public String dashboardView(@AuthenticationPrincipal CustomUserDetails user, Model model) {
+	  int memNo = user.getMemNo();
+	  // 회사번호를 가져오는 코드가 현재 없어서 임시로 해결. 나중에 기업회원 테이블 및 회사 정보 테이블 조회하는 코드 필요
+		/* int compNo= user.getCompNo(); */
+	  System.out.println("memNo=" + user.getMemNo());
+		/* System.out.println("compNo=" + user.getCompNo()); */
+	  System.out.println("principal=" + user);
+
+	  
+	  CompanyDashboardVO dashboard = service.getDashboard(memNo);
+	  model.addAttribute("dashboard", dashboard);
+	  List<PaymentVO> paymentList = service.selectPaymentList(memNo);
+	  model.addAttribute("paymentList", paymentList);
+	  
+	  System.out.println("dashboard : " + dashboard);
+	
+	  return "mypage/business/dashboard";
   }
   
   @GetMapping("/mypage/business/notifications")
--- src/main/java/kr/or/ddit/mohaeng/mypage/dashboard/service/CompanyDashboardServiceImpl.java
+++ src/main/java/kr/or/ddit/mohaeng/mypage/dashboard/service/CompanyDashboardServiceImpl.java
@@ -1,9 +1,12 @@
 package kr.or.ddit.mohaeng.mypage.dashboard.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import kr.or.ddit.mohaeng.mypage.dashboard.mapper.ICompanyDashboardMapper;
 import kr.or.ddit.mohaeng.vo.CompanyDashboardVO;
+import kr.or.ddit.mohaeng.vo.PaymentVO;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -14,10 +17,26 @@ public class CompanyDashboardServiceImpl implements CompanyDashboardService {
 
   @Override
   public CompanyDashboardVO getDashboard(int compNo) {
+	  CompanyDashboardVO.Kpi kpi = mapper.selectKpi(compNo);
+	 
+	 
+
     CompanyDashboardVO vo = new CompanyDashboardVO();
     vo.setKpi(mapper.selectKpi(compNo));
-    vo.setMonthlySalesChart(mapper.selectMonthlySalesChart(compNo, 6));
-    vo.setTopProducts(mapper.selectTopProducts(compNo, 5));
+    System.out.println("🔥 KPI = " + kpi);
+    System.out.println("compNo=" + compNo);
+
+	/*
+	 * vo.setMonthlySalesChart(mapper.selectMonthlySalesChart(compNo, 6));
+	 * vo.setTopProducts(mapper.selectTopProducts(compNo, 5));
+	 */
     return vo;
   }
+
+  @Override 
+  public List<PaymentVO> selectPaymentList(int memNo) {
+	  System.out.println("memNo=" + memNo);
+	return mapper.selectPaymentList(memNo);
+  }
+ 
 }

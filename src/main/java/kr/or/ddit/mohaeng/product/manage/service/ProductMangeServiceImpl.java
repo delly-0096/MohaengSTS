6ab094a1cca4fb5cfabbc4fe2392c3db3e4ac45e
donggeun <dongkun8130@gmail.com>
2026-01-22 09:39:26 +0900

    [BE] 신동근 / business 패키지 -> product.manage 패키지로 변경, 클래스 명들도 똑같이.

---
+++ src/main/java/kr/or/ddit/mohaeng/product/manage/service/ProductMangeServiceImpl.java
@@ -0,0 +1,204 @@
+package kr.or.ddit.mohaeng.product.manage.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.or.ddit.mohaeng.ServiceResult;
+import kr.or.ddit.mohaeng.accommodation.service.IAccommodationService;
+import kr.or.ddit.mohaeng.business.mapper.IBusinessProductMapper;
+import kr.or.ddit.mohaeng.tour.vo.ProdTimeInfoVO;
+import kr.or.ddit.mohaeng.tour.vo.TripProdInfoVO;
+import kr.or.ddit.mohaeng.tour.vo.TripProdPlaceVO;
+import kr.or.ddit.mohaeng.tour.vo.TripProdSaleVO;
+import kr.or.ddit.mohaeng.tour.vo.TripProdVO;
+import kr.or.ddit.mohaeng.vo.AccommodationVO;
+import kr.or.ddit.mohaeng.vo.AttachFileDetailVO;
+import kr.or.ddit.mohaeng.vo.BusinessProductsVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class ProductMangeServiceImpl implements IProductMangeService {
+
+
+	// 투어같은 상품 조회
+	@Autowired
+	private IBusinessProductMapper businessMapper;
+	
+	@Override
+	public List<BusinessProductsVO> getProductlist(BusinessProductsVO businessProducts) {
+		return businessMapper.getProductlist(businessProducts);
+	}
+	
+	@Override
+	public List<AccommodationVO> getAccommodationList(BusinessProductsVO businessProducts) {
+		return businessMapper.getAccommodationList(businessProducts);
+	}
+	
+	@Override
+	public TripProdVO getProductAggregate(TripProdVO tripProd) {
+		return businessMapper.getProductAggregate(tripProd);
+	}
+	
+	@Override
+	public BusinessProductsVO getProductDetail(BusinessProductsVO businessProducts) {
+		
+		// 상품 정보, 상품 이용안내, 상품 가격, 여행 상품 관광지
+		BusinessProductsVO prodVO = businessMapper.retrieveProductDetail(businessProducts);
+		log.info("retrieveProductDetail-retrieveProductDetail : {}", prodVO);
+
+		// 숙소 타입일 경우에는 이렇게
+		if(prodVO.getProdCtgryType().equals("accommodation")) {
+			int accNo = 0;
+			if(businessProducts.getAccNo() != 0) {
+				accNo = businessProducts.getAccNo();
+			}
+			
+			AccommodationVO accommodationvo = new AccommodationVO();
+			accommodationvo.setAccNo(accNo);
+			
+			AccommodationVO accommodation = businessMapper.retrieveAccomodationDetail(accommodationvo);
+			prodVO.setAccommodation(accommodation);
+			log.info("retrieveAccomodationDetail : {}", accommodation.getRoomTypeList().size());
+			
+			// 숙소용 사진은 accommodation에 있음
+			// accommodation에 있는 것
+			
+		// 숙소가 아닐때는 이렇게
+		} else {
+			List<ProdTimeInfoVO> prodTimeList = businessMapper.retrieveProdTimeList(prodVO);
+			log.info("retrieveProductDetail-retrieveProdTimeList : {}", prodTimeList);
+			
+			if(prodTimeList != null && prodTimeList.size() > 0) {
+				prodVO.setProdTimeList(prodTimeList);
+				log.info("prod setProdTimeList : {}", prodVO.getProdTimeList());
+			}
+			
+			
+			List<AttachFileDetailVO> productImages = new ArrayList<>();
+			
+			// 상품 사진
+			if (prodVO.getAttachNo() != null && prodVO.getAttachNo() > 0) {
+				productImages = businessMapper.retrieveProdImages(prodVO);
+				log.info("getAttachFileDetails : {}", productImages);
+			}
+			
+			if(productImages != null && productImages.size() > 0) {
+				prodVO.setImageList(productImages);
+			}
+		}
+		
+		
+		return prodVO;
+	}
+	
+	@Override
+//	@Transactional
+	public ServiceResult modifyProduct(BusinessProductsVO businessProducts) {
+		ServiceResult result = null;
+		
+		int tripProdNo = businessProducts.getTripProdNo();
+		// 변수 바뀔수도, 1대1관계들 update -> 상품, 상품 이용안내, 상품 가격, 상품 장소
+		// 상품
+		int productstatus = 0;	// 1 , 0
+		productstatus = businessMapper.modifyTripProduct(businessProducts);
+		log.info("productstatus : {}", productstatus);
+		
+		// 상품 가격
+		int saleStatus = 0;
+		TripProdSaleVO prodSaleVO = businessProducts.getProdSale();
+		prodSaleVO.setTripProdNo(tripProdNo);
+		saleStatus = businessMapper.modifyProdSale(prodSaleVO);
+		log.info("saleStatus : {}", saleStatus);
+		
+		// 상품 이용안내
+		int infoStatus = 0;
+		TripProdInfoVO prodInfoVO = businessProducts.getProdInfo();
+		prodInfoVO.setTripProdNo(tripProdNo);
+		infoStatus = businessMapper.modifyProdInfo(prodInfoVO);
+		log.info("infoStatus : {}", infoStatus);
+		
+		// 상품 장소
+		int placeStatus = 0;
+		TripProdPlaceVO prodPlaceVO = businessProducts.getProdPlace();
+		prodPlaceVO.setTripProdNo(tripProdNo);
+		placeStatus = businessMapper.modifyProdPlace(prodPlaceVO);
+		log.info("placeStatus : {}", placeStatus);
+		
+		// 결과 비교
+		int baseStatus = productstatus + saleStatus + infoStatus + placeStatus;	// 4
+		// 숙소 
+		
+		// 예약가능시간(예약 가능 시간)
+		int productTimetatus = 0;
+		productTimetatus = businessMapper.deleteProdTimeInfo(businessProducts);
+		log.info("deleteProdInfo : {}", productTimetatus);
+		
+		// 예약시간 수정
+		List<ProdTimeInfoVO> prodTimeInfoVO = businessProducts.getProdTimeList();
+		log.info("prodTimeInfoVO.길이 : {}", prodTimeInfoVO);
+		boolean isTimeSuccess = true; // 시간 처리 성공 여부 플래그
+		if (prodTimeInfoVO != null && !prodTimeInfoVO.isEmpty()) {
+			for(ProdTimeInfoVO timeInfo : prodTimeInfoVO) {
+				timeInfo.setTripProdNo(tripProdNo);
+			}
+			
+			int insertCount = businessMapper.insertProdTimeInfo(prodTimeInfoVO);
+			log.info("시간 등록 개수: {}, 기대 개수: {}", insertCount, prodTimeInfoVO.size());
+			
+			if (insertCount != prodTimeInfoVO.size()) {
+				isTimeSuccess = false;
+			}
+		}
+
+		if (baseStatus == 4 && isTimeSuccess) {
+	        log.info("수정 최종 성공");
+	        return ServiceResult.OK;
+	    } else {
+	        log.error("수정 실패");
+	        return ServiceResult.FAILED;
+	    }
+		
+		// 숙소 정보는 나중에
+//		int accomStatus = 0;
+		
+//		return null;
+	}
+	
+	@Override
+	public ServiceResult updateProductStatus(TripProdVO tripProd) {
+		ServiceResult result = null;
+		log.info("updateProductStatus 실행 : {}", tripProd);
+		if (tripProd.getApproveStatus().equals("판매중")) {
+			tripProd.setApproveStatus("판매중지");
+		} else {
+			tripProd.setApproveStatus("판매중");
+		}
+		
+		int status = businessMapper.updateProductStatus(tripProd);
+		log.info("status : {}", status);
+		if(status > 0) {
+			return result = ServiceResult.OK;
+		} else {
+			return result = ServiceResult.FAILED;
+		}
+	}
+
+	@Override
+	public ServiceResult deleteProductStatus(TripProdVO tripProd) {
+		ServiceResult result = null;
+		log.info("updateProductStatus 실행 : {}", tripProd);
+		
+		int status = businessMapper.deleteProductStatus(tripProd);
+		log.info("status : {}", status);
+		if(status > 0) {
+			return result = ServiceResult.OK;
+		} else {
+			return result = ServiceResult.FAILED;
+		}
+	}
+
+}

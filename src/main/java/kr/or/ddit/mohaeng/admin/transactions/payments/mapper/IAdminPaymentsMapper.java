7b5cab2ba3466efd17985312a8ae75aa27ca8acf
jisu <wltn7235@gmail.com>
2026-01-28 18:54:24 +0900

    [BE] 하지수 / 관리자결제내역 완료, 여행기록 시연버튼 생성

--- src/main/java/kr/or/ddit/mohaeng/admin/transactions/payments/mapper/IAdminPaymentsMapper.java
+++ src/main/java/kr/or/ddit/mohaeng/admin/transactions/payments/mapper/IAdminPaymentsMapper.java
@@ -4,9 +4,14 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 import kr.or.ddit.mohaeng.vo.AdminPaymentsVO;
 import kr.or.ddit.mohaeng.vo.PaginationInfoVO;
+
 @Mapper
 public interface IAdminPaymentsMapper {
     Map<String, Object> selectAdminPaymentStats();
     int selectAdminPaymentsCount(PaginationInfoVO<AdminPaymentsVO> pagingVO);
     List<AdminPaymentsVO> selectAdminPaymentsList(PaginationInfoVO<AdminPaymentsVO> pagingVO);
+    
+    // 상세 조회 추가
+    Map<String, Object> selectPaymentDetail(int payNo);
+    List<Map<String, Object>> selectReceiptDetailList(int payNo);
 }
--- src/main/java/kr/or/ddit/mohaeng/admin/transactions/payments/service/AdminPaymentsServiceImpl.java
+++ src/main/java/kr/or/ddit/mohaeng/admin/transactions/payments/service/AdminPaymentsServiceImpl.java
@@ -1,24 +1,38 @@
 package kr.or.ddit.mohaeng.admin.transactions.payments.service;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import kr.or.ddit.mohaeng.admin.transactions.payments.mapper.IAdminPaymentsMapper;
 import kr.or.ddit.mohaeng.vo.AdminPaymentsVO;
 import kr.or.ddit.mohaeng.vo.PaginationInfoVO;
+
 @Service
 public class AdminPaymentsServiceImpl implements IAdminPaymentsService {
+    
     @Autowired
     private IAdminPaymentsMapper adminMapper;
+    
     @Override
     public Map<String, Object> getAdminPaymentDashboard() {
         return adminMapper.selectAdminPaymentStats();
     }
+    
     @Override
     public PaginationInfoVO<AdminPaymentsVO> getAdminPaymentsList(PaginationInfoVO<AdminPaymentsVO> pagingVO) {
         int totalRecord = adminMapper.selectAdminPaymentsCount(pagingVO);
         pagingVO.setTotalRecord(totalRecord);
         pagingVO.setDataList(adminMapper.selectAdminPaymentsList(pagingVO));
         return pagingVO;
+    }
+    
+    @Override
+    public Map<String, Object> getPaymentDetail(int payNo) {
+        return adminMapper.selectPaymentDetail(payNo);
+    }
+    
+    @Override
+    public List<Map<String, Object>> getReceiptDetailList(int payNo) {
+        return adminMapper.selectReceiptDetailList(payNo);
     }
 }
